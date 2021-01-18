625393b6142512af18bc430f6cd5c02c4e76ecd6
rick_marcos <rick.marcos@astoria.com.ph>
2021-01-18 13:50:19 +0800

    Modification before fix

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { Member } from '../_models/member';
 
@@ -8,7 +9,7 @@ import { Member } from '../_models/member';
 })
 export class MembersService {
   baseUrl = environment.apiUrl;
-
+  members: Member[] = [];
   constructor(private http: HttpClient) { }
 
   getMembers() {
@@ -21,5 +22,14 @@ export class MembersService {
 
   getMemberById(id: number) {
     return this.http.get<Member>(this.baseUrl + 'users/' + id);
+  }
+
+  updateMember(member: Member) {
+    return this.http.put(this.baseUrl + 'users', member).pipe(
+      map(() => {
+        const index = this.members.indexOf(member);
+        this.members[index] = member;
+      })
+    );
   }
 }
