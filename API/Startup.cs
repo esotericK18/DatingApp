867928b510c2d82f0fab21a5aa366b9868ae7ac8
rick_marcos <rick.marcos@astoria.com.ph>
2021-02-10 12:42:31 +0800

    added heroku to cors

--- API/Startup.cs
+++ API/Startup.cs
@@ -69,7 +69,7 @@ namespace API
             app.UseCors(x => x.AllowAnyHeader()
                 .AllowAnyMethod()
                 .AllowCredentials()
-                .WithOrigins("https://localhost:4200"));
+                .WithOrigins("https://localhost:4200", "https://rick-datingapp.herokuapp.com"));
 
             app.UseAuthentication();
             app.UseAuthorization();
